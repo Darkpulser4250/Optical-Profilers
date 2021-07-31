34a5a10d2e0b9f1ea4b7d480e9afa116b41763ae
Muhammad Daud <62312978+Darkpulser4250@users.noreply.github.com>
2021-07-31 01:44:41 -0500

    Add files via upload

---
+++ pdf_reader.py
@@ -0,0 +1,55 @@
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from flask import Flask, render_template, request, redirect
+
+
+# add flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('UI.html')
+
+def pdf_extracter(fileName):
+    filesize = os. path. getsize("notes.txt") 
+    if filesize == 0: 
+        print("The file is empty: " + str(filesize))
+        vr_doc = ["Texas Voter Registration Application"]
+        ballot = []
+
+        file_path = fileName
+        pdf = PdfFileReader(file_path)
+
+        with open('notes.txt', 'w') as f:
+            for page_num in range(pdf.numPages):
+                print('Page: {0}'.format(page_num))
+                pageObj = pdf.getPage(page_num)
+
+                try: 
+                    txt = pageObj.extractText()
+                    print(''.center(100, '-'))
+                except:
+                    pass
+                else:
+                    f.write('Page {0}\n'.format(page_num+1))
+                    f.write(''.center(100, '-'))
+                    f.write(txt)
+            f.close()
+            f = open("notes.txt", "r")
+            # print(f.read())
+            if vr_doc[0] in f.read():
+                print("Saved in the Texas Voter Registration Application folder")
+            else:
+                print("NOT")
+        
+    else: 
+        print("File not Empty, Truncating.....")
+        with open("notes.txt", 'r+') as f:
+            f.truncate(0)
+            print("Truncating.....COMPLETE!!!")
+    
+    
+if __name__ == "__main__":
+    pdf_extracter("vr_app.pdf")
+    # app.run(debug=True)
